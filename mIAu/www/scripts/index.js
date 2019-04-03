2412962a87b851f7638a10378ed933de006fa3c8
kPlusPlus <kresimir.ivkovic@gmail.com>
2019-04-03 23:55:42 +0200

    test 1 - ajax not working on android

--- mIAu/www/scripts/index.js
+++ mIAu/www/scripts/index.js
@@ -1,15 +1,32 @@
-﻿/*
+﻿
 $(document).on('pagecreate', '#app', function () {
-    alert('Network error has occurred please try again!');
+    //alert('Network error has occurred please try again!');
+    //alert("START NOW");
+    //TakeAll();
+});
+
+
+$(document).on("tap", "#btnCountry", function () {    
+    //alert("hehehe");
+    TakeCountry();
+    //alert("ALL RIGHT");
+});
+
+/*
+$("#btnCountry").tap(function () {
+    //alert("H1 234");
+    //TakeAll();
+    TakeCountry();
 });
 */
 
-$(document).on("tap", "#test", function () {
+$("#btnTest").tap(function () {
+    alert("RRR");
     TakeAll();
-});
+})
 
 $(document).on("change", "#selcountry", function () {
-    //alert("RADIM");
+    //alert("WORKING...");
     var codecountry;
     codecountry = $("#selcountry").select().val();
     TakeCity(codecountry);
@@ -17,6 +34,11 @@ $(document).on("change", "#selcountry", function () {
 
 
 function TakeAll() {
+
+    //$("#divcity").empty();
+    //$("#divcountry").empty();
+
+    /*
     $.get("http://159.69.113.252/~kapluspl/tmp/citieslist.php", function (data) {
         $("#divcity").html(data);
         //alert("Load was performed.");
@@ -24,7 +46,30 @@ function TakeAll() {
 
     $.get("http://159.69.113.252/~kapluspl/tmp/countrylist.php", function (data) {
         $("#divcountry").html(data);
-    });   
+    });
+    */
+
+    $.ajax({
+        url: 'http://159.69.113.252/~kapluspl/tmp/countrylist.php',
+        cache: false,
+        success: function (data) {
+            $("#divcountry").html(data);
+        },
+        fail: function (data) {
+            alert('Err 4. Country list');
+        }
+    });
+
+}
+
+function TakeCountry()
+{
+    //$("#divcountry").empty();
+    $.get("http://159.69.113.252/~kapluspl/tmp/countrylist.php", function (data) {
+        $("#divcountry").html(data);
+        alert('Load was performed.');
+    });
+
 }
 
 function TakeCity(codecountry) {
@@ -32,9 +77,12 @@ function TakeCity(codecountry) {
         url: 'http://159.69.113.252/~kapluspl/tmp/citieslist.php',
         data: { "country" : codecountry },              // $('#formA').serialize(),
         type: "POST",
-        success: function (data) {
-            //$('.result').html(data);
-            //alert('Load was performed.');
+        success: function (data) {            
+            $("#divcity").html(data);            
+        },
+        fail: function (data) {
+            alert('Err 6. Internet connection problem');
         }
+        
     });   
 }
