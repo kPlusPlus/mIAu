00f3c838cc6bd1e89bf6f1e2ca8a9fe67ee36ca1
kPlusPlus <kresimir.ivkovic@gmail.com>
2019-04-03 12:01:39 +0200

    country, citier

--- mIAu/www/scripts/index.js
+++ mIAu/www/scripts/index.js
@@ -1,45 +1,40 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
-(function () {
-    "use strict";
+﻿/*
+$(document).on('pagecreate', '#app', function () {
+    alert('Network error has occurred please try again!');
+});
+*/
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+$(document).on("tap", "#test", function () {
+    TakeAll();
+});
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        /*
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        var changeColorButton = document.querySelector("#changeColor");
-        changeColorButton.addEventListener("click", setRandomColor, false);
-        */
-    };
-
-    function setRandomColor(e) {
-        alert("eeee123");
-    };
-
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
-
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-
-    
+$(document).on("change", "#selcountry", function () {
+    //alert("RADIM");
+    var codecountry;
+    codecountry = $("#selcountry").select().val();
+    TakeCity(codecountry);
+});
 
 
+function TakeAll() {
+    $.get("http://159.69.113.252/~kapluspl/tmp/citieslist.php", function (data) {
+        $("#divcity").html(data);
+        //alert("Load was performed.");
+    });
 
+    $.get("http://159.69.113.252/~kapluspl/tmp/countrylist.php", function (data) {
+        $("#divcountry").html(data);
+    });   
+}
 
-} )();
+function TakeCity(codecountry) {
+    $.ajax({
+        url: 'http://159.69.113.252/~kapluspl/tmp/citieslist.php',
+        data: { "country" : codecountry },              // $('#formA').serialize(),
+        type: "POST",
+        success: function (data) {
+            //$('.result').html(data);
+            //alert('Load was performed.');
+        }
+    });   
+}
